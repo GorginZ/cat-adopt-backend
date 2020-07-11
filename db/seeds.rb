e8640dd5bc764d4905538bc8ba71340f360767c8
GorginZ <georgia.z.leng@gmail.com>
2020-07-11 15:20:06 +1000

    seed file cats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.create(
+    name: "Brad",
+    sex: "male",
+    age: 3,
+    bio: "I'm a friendly cat who does well with children. I love to play and am an indoor cat. I'm a longhair so I might need some extra care",
+    state: "Victoria",
+    suburb: "Frankston"
+
+)
+
+Cat.create(
+    name: "Spot",
+    sex: "female",
+    age: 2,
+    bio: "I'm young and love to play. I might not mind being around another cat or dog and am very friendly. I am de-sexed.",
+    state: "Victoria",
+    suburb: "Brunswick West"
+
+)
+Cat.create(
+    name: "Tim",
+    sex: "male",
+    age: 6,
+    bio: "I'm a very friendly indoor cat. I don't mind other cats and am very mature and patient. I will be very gentle with children. I am de-sexed.",
+    state: "Victoria",
+    suburb: "Clifton Hill"
+
+)
+
